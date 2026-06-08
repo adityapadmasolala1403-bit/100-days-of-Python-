bb6f88b9dc4945ec6dcc6aefdf588ce46b804c01
Aditya <adityapadmasolala1403@gmail.com>
2026-06-08 11:54:08 +0530

    Create Day-1 Band name Generator.py
    This mini project reflects my learning of  string concatenation and f-strings.

---
+++ Daily Projects/Day-1 Band name Generator.py
@@ -0,0 +1,9 @@
+print("BAND NAME GENERATOR")
+
+user_locality=str(input("Enter your locality name :"))
+
+user_pet_name=str(input("Enter your pet's name:"))
+
+band_name=f'Your band name could be:{user_locality+" "+user_pet_name}'
+
+print(band_name)
